3b243f50dda1b61c3dd207df824fa02263e42133
BrahimJandri <bzaza066@gmail.com>
2025-12-15 14:30:34 +0100

    start coding html

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -3,6 +3,14 @@ export default function NavBar() {
     <>
     <nav>
       <h1>NavBar Component</h1>
+      <ul>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Our Work</a>
+        <a href="#">Contact</a>
+      </ul>
+      
     </nav>
     </>
   )
